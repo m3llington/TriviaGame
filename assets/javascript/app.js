fb84bd8b107ce631a48e7c7fdc6ff18ae9b050fc
m3llington <mellingtonucf@knights.ucf.edu>
2019-07-12 20:09:11 -0400

    added buttons, and functionality on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // console.log(triviaQuestions);
-let counter = 15;
+let counter = 2;
 // keeps track of current question. Starts at 0 because thats the first index an a array
 let currentQuestion = 0;
 let correct = 0;
@@ -8,10 +8,32 @@ let incorrect = 0;
 let timer;
 
 //Display question and choices into the html
-function stop(){
-    clearInterval(timer);
+$("#start").on("click",  function () {
+
+    $(".jumbotron").html("");
+//loads the next question, once timer has reached 0 or an answer has been selected
+function nextQuestion() {
+    //logic that lets function know its on the last question and to not run forever
+    //you do -1 to account for how index's work in arrays
+    const isQuestionOver = (triviaQuestions.length - 1) === currentQuestion;
+    if (isQuestionOver) {
+        console.log("game is over");
+        displayResult();
+    } else {
+        currentQuestion++;
+        displayQuestion();
+    }
+    // currentQuestion++;
+    // displayQuestion();
 }
 
+//stops the timer once it reaches 0
+function stop(){
+    clearInterval(timer);
+    incorrect++;
+    nextQuestion();
+}
+//timer function, the counter increments, the timer is whats displayed
 function increment(){
     
     counter--;
@@ -22,8 +44,9 @@ function increment(){
     }
 }
 
+//loads the first question
 function displayQuestion() {
-    counter = 15;
+    counter = 2;
     timer = setInterval(increment, 1000);
 
   const question = triviaQuestions[currentQuestion].question;  //create new variable to hold the current question
@@ -39,7 +62,7 @@ function displayQuestion() {
 }
 
 function displayOptions(options){
-    //
+    //loads the options in function stores the results in let result
     let result = "";
 
     for (let i = 0; i < options.length; i++){
@@ -50,6 +73,36 @@ function displayOptions(options){
     return result;
 
 }
+//this is important since you can not eventlisten to something that was created outside of the dom
+$("#game").on("click", ".choice", function() {
+    clearInterval(timer);
+    //stores the value of "this" which is whatever element was clicked
+    const selectedAnswer = $(this).attr("data-answer");
+    const correctAnswer = triviaQuestions[currentQuestion].answer;
 
+    if (correctAnswer === selectedAnswer){
+        correct++;
+        console.log("correct");
+        nextQuestion();
+    } else{
+        incorrect++;
+        console.log("incorect");
+        nextQuestion();
+    }
+    
+    console.log(selectedAnswer);
+});
+function displayResult() {
+    const result  = `
+    <p>Correct answers: ${correct}</p>
+    <p>Incorrect, or unanswered: ${incorrect}</p>
+    <button type="button" class="btn btn-primary" id="reset">Reset Game</button>
+    `;
+
+    $("#game").html(result);
+
+}
 displayQuestion();
+
+});
 // displayOptions();
